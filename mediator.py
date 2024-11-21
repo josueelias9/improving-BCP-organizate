3ce799121dfbf7534b8cf8bdadfa99a0be6d0252
josue <josueeliashuaman@gmail.com>
2024-11-21 14:47:49 -0500

    feat: adding pictures for each month

--- mediator.py
+++ mediator.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 story_table_str = "./db/story.csv"
 transaction_table_str = "./db/transaction.csv"
-story_columns = ["story", "new_category", "requires_update", "date_id"]
+story_columns = ["date_id", "story", "new_category", "requires_update"]
 
 
 def create_merged_df():
@@ -62,6 +62,10 @@ story.csv         EXISTS """,
     }
 
 
+def get_months():
+    return pd.read_csv("./db/transaction.csv")["month"].unique()
+
+
 def get_categories():
     return pd.read_csv("./db/category.csv")["category"].to_list()
 
@@ -71,40 +75,63 @@ def ETL_transaction(string_data):
 
 
 def make_graphs(edited_df):
-    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    fig, axs = plt.subplots(3, 3, figsize=(20, 20))
+
+    # only expenses, only october, no TRAN.CTAS.PROP.BM
+    filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["month"] == 7) & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
+    alfa = filtered_df[["new_category", "amount"]].groupby(["new_category"]).sum().reset_index()
+    axs[0, 0].set_title("july")
+    axs[0, 0].bar(alfa["new_category"], alfa["amount"])
+    axs[0, 0].set_xticklabels(alfa["new_category"], rotation=90)
+    axs[0, 0].grid(True)
+
+    # only expenses, only october, no TRAN.CTAS.PROP.BM
+    filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["month"] == 8) & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
+    alfa = filtered_df[["new_category", "amount"]].groupby(["new_category"]).sum().reset_index()
+    axs[0, 1].set_title("august")
+    axs[0, 1].bar(alfa["new_category"], alfa["amount"])
+    axs[0, 1].set_xticklabels(alfa["new_category"], rotation=90)
+    axs[0, 1].grid(True)
 
     # only expenses, only october, no TRAN.CTAS.PROP.BM
     filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["month"] == 9) & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
     alfa = filtered_df[["new_category", "amount"]].groupby(["new_category"]).sum().reset_index()
-    axs[0, 0].set_title("september")
-    axs[0, 0].bar(alfa["new_category"], alfa["amount"])
-    axs[0, 0].set_xticklabels(alfa["new_category"], rotation=90)  # Rotar etiquetas del eje x en el primer subplot
-    axs[0, 0].grid(True)
+    axs[0, 2].set_title("september")
+    axs[0, 2].bar(alfa["new_category"], alfa["amount"])
+    axs[0, 2].set_xticklabels(alfa["new_category"], rotation=90)
+    axs[0, 2].grid(True)
 
     # only expenses, only november, no TRAN.CTAS.PROP.BM
     filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["month"] == 10) & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
     alfa = filtered_df[["new_category", "amount"]].groupby(["new_category"]).sum().reset_index()
-    axs[0, 1].set_title("october")
-    axs[0, 1].bar(alfa["new_category"], alfa["amount"])
-    axs[0, 1].set_xticklabels(alfa["new_category"], rotation=90)  # Rotar etiquetas del eje x en el primer subplot
-    axs[0, 1].grid(True)
+    axs[1, 0].set_title("october")
+    axs[1, 0].bar(alfa["new_category"], alfa["amount"])
+    axs[1, 0].set_xticklabels(alfa["new_category"], rotation=90)
+    axs[1, 0].grid(True)
 
     # only expenses, only november, no TRAN.CTAS.PROP.BM
     filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["month"] == 11) & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
     alfa = filtered_df[["new_category", "amount"]].groupby(["new_category"]).sum().reset_index()
-    axs[1, 0].set_title("november")
-    axs[1, 0].bar(alfa["new_category"], alfa["amount"])
-    axs[1, 0].set_xticklabels(alfa["new_category"], rotation=90)  # Rotar etiquetas del eje x en el primer subplot
-    axs[1, 0].grid(True)
+    axs[1, 1].set_title("november")
+    axs[1, 1].bar(alfa["new_category"], alfa["amount"])
+    axs[1, 1].set_xticklabels(alfa["new_category"], rotation=90)
+    axs[1, 1].grid(True)
 
     # expenses by month
     filtered_df = edited_df[(edited_df["type"] == "expense") & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
     alfa = filtered_df[["amount", "month"]].groupby(["month"]).sum().reset_index()
-    axs[1, 1].set_title("total expenses by month")
-    axs[1, 1].bar(alfa["month"], alfa["amount"])
-    axs[1, 1].grid(True)
+    axs[1, 2].set_title("total expenses by month")
+    axs[1, 2].bar(alfa["month"], alfa["amount"])
+    axs[1, 2].grid(True)
 
-    plt.subplots_adjust(hspace=1)
+    # income
+    filtered_df = edited_df[(edited_df["type"] == "income") & (edited_df["description"] != "TRAN.CTAS.PROP.BM")]
+    alfa = filtered_df[["amount", "month"]].groupby(["month"]).sum().reset_index()
+    axs[2, 0].set_title("total income by month")
+    axs[2, 0].bar(alfa["month"], alfa["amount"])
+    axs[2, 0].grid(True)
+
+    plt.subplots_adjust(hspace=0.6)
     return fig
 
 
@@ -119,7 +146,10 @@ def save_df(edited_df):
 
     now = datetime.now().strftime("%Y-%m-%d %H:%M")
     edited_df[story_columns].to_csv(story_table_str, index=False)
-    edited_df[story_columns].to_csv(f"/mnt/c/Users/josue/Desktop/reports/personal information {now}.csv", index=False)
+    edited_df[story_columns].to_csv(
+        f"/mnt/c/Users/josue/Desktop/reports/personal information {now}.csv",
+        index=False,
+    )
 
 
 def update_requires_update_column(edited_df):
